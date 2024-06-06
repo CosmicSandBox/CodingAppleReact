b3abf73da05cc3f9da0e2780624c9c137383aea1
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2024-06-06 20:55:06 +0900

    코딩애플 react 좋아요 수 증가 및 오름차순 정렬 버튼 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let post = '강남 우동 맛집';
+    let [글제목, setTitle] = useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
+    let [logo, setLogo] = useState('ReactBlog');
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+
+                <h4>블로그임</h4>
+åå
+                <button onClick={()=>{
+                    const copy = [...글제목];
+                    copy[0] = '여자코트 추천';
+                    setTitle(copy);
+                    }}>눌러봐라</button>
+
+                <button onClick={()=>{
+                    const ascending = [...글제목];
+                    setTitle(ascending.sort());
+                }}>오름차순 정렬</button>
+
+            </div>
+            <div className="list">
+                <h4>{ 글제목[0] } <span onClick={()=>{setCount(count+1)}}>👍</span>  {count} </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{ 글제목[1] } <span>👍</span>  {count} </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{ 글제목[2] } <span>👍</span>  {count} </h4>
+                <p>2월 17일 발행</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
